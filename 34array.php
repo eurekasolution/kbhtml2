0554beb07d4d74011b835789317bc77da9984ac2
eurekasolution <nadopro@gmail.com>
2022-03-21 10:53:07 +0900

    2022-03-21 11:00

--- 34array.php
+++ 34array.php
@@ -29,5 +29,55 @@
     lotto = lotto.concat(seasons);
     console.log("lotto = " + lotto);
 
+    // stack : 동전통 FILO, LIFO
+    //          push(), pop()
+    // queue : q , FIFO, LILO
+    //          enqueue(), dequeue()
+    //      add(), remove()
+
+    console.log("lotto = " + lotto);
+    lotto.pop();
+    console.log("lotto = " + lotto);
+    lotto.shift();
+    console.log("lotto = " + lotto); // 맨 앞이 빠져나감.
+
+    let x = 5;      // 1010
+    x = x << 1;
+    console.log("x = " + x);
+
+    lotto = lotto.slice(2);
+    console.log("lotto = " + lotto);
+    //lotto = 2,3,4,10,봄,여름,가을
+    // lotto = 4,10,봄,여름,가을
+    
+    function checkError()
+    {
+        // 기존 방법
+        var f = document.loginForm;
+        //alert(f.id.value); // document.loginForm.id.value;
+        var id = document.querySelector("#id");
+       
+        var regex = /^[a-zA-Z0-9]{4,10}$/;
+        if(!regex.test(id.value))
+        {
+            alert("아이디는 영어 대소문자및 숫자로 4~10자입니다.");
+            id.focus();
+            return false;
+        }
+
+    }
 
 </script>
+
+
+<form name="loginForm" method="post" onSubmit="return checkError()" action="a.html">    <!-- GET, POST, PUT, DELETE -->
+<div class="row">
+    <div class="col-2 colLine">아이디</div>
+    <div class="col colLine">
+        <input type="text" name="id" id="id" required class="form-control">
+    </div>
+</div>
+
+<button type="submit" class="btn btn-primary">로그인</button>
+
+</form>
