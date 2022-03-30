0aef52ccb7df95e6b91f2c7f6f24241254a4b76e
eurekasolution <nadopro@gmail.com>
2022-03-30 16:21:37 +0900

    2022-03-30 16:20

--- 58insert.php
+++ 58insert.php
@@ -115,7 +115,11 @@
 <script>
     function confirmDelete(pidx)
     {
-        location.href='main.php?cmd=58insert&mode=delete&idx='+pidx;
+        if(confirm('삭제된 데이터는 복구할 수 없습니다.\n정말 삭제하시겠습니까?'))
+        {
+            location.href='main.php?cmd=58insert&mode=delete&idx='+pidx;
+        }
+        
     }
 </script>
 
@@ -128,7 +132,35 @@
 </div>
 
 <?php
-    $sql = "SELECT * FROM dept ORDER BY name ASC";
+    $sql = "SELECT count(*) as total  FROM dept ORDER BY name ASC";
+    $result = mysqli_query($conn, $sql);
+    $data = mysqli_fetch_array($result);
+
+    $total = $data["total"];
+    echo "total = $total <br>";
+    $LPP = 10; // total/ 3
+    $totalPage = ceil($total / $LPP);
+    echo "totalPage = $totalPage <br>";
+
+    if(isset($_GET["page"]))  // main.php?cmd=58input&page=3
+    {
+        $page = $_GET["page"];
+    }
+
+    if(!isset($page))
+        $page = 1;
+
+    /*
+        1p  : 0 ~3
+        2p  : 3 ~3
+        3p  : 6 ~3
+        n   :(n-1)*3
+    */
+
+    $start = ($page -1) * $LPP;
+
+
+    $sql = "SELECT * FROM dept ORDER BY name ASC LIMIT $start, $LPP";
     $result = mysqli_query($conn, $sql);
     $data = mysqli_fetch_array($result);
 
@@ -156,4 +188,21 @@
         <?php
         $data = mysqli_fetch_array($result);
     }
+
+    ?>
+    <div class="row">
+        <div class="col colLine text-center">
+        <?php
+            for($i=1; $i<=$totalPage; $i++)
+            {
+                if($i == $page)
+                    echo "<a href='main.php?cmd=58insert&page=$i'><span class='badge bg-danger'>$i</span></a> ";
+                else
+                    echo "<a href='main.php?cmd=58insert&page=$i'><span class='badge bg-secondary'>$i</span></a> ";
+            }
+        ?>
+        </div>
+    </div>
+    <?php
+
 ?>
