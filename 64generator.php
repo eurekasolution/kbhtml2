55fdadb0b4064bac286c63861e81a98c411ad787
eurekasolution <nadopro@gmail.com>
2022-04-01 09:50:38 +0900

    2022-04-01 1000

--- 64generator.php
+++ 64generator.php
@@ -5,12 +5,24 @@
 </div>
 
 <?php
-    $rand = rand(200, 300)/ 10;
+    $temperature = rand(200, 300)/ 10;
+    $humidity = rand(500, 700)/ 10;
+
+    $sql = "INSERT INTO iot (branch, temperature, humidity, time) 
+                    VALUES('1', '$temperature', '$humidity', now() )";
+    $result = mysqli_query($conn, $sql);
 ?>
 
 
 <div class="row">
     <div class="col colLine">
-        rand = <?php echo $rand ?>
+        temperature = <?php echo $temperature ?> <br>
+        humidity = <?php echo $humidity ?> 
     </div>
 </div>
+
+<script>
+    setTimeout( function(){
+        location.href='main.php?cmd=64generator';
+    }   , 5000);
+</script>
