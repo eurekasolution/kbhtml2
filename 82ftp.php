393b612b83813a48f313f9b6968a22191024223f
eurekasolution <nadopro@gmail.com>
2022-05-03 11:46:11 +0900

    2022-05-03 11:50

--- 82ftp.php
+++ 82ftp.php
@@ -2,24 +2,100 @@
 
     function getDirs($path)
     {
+        echo "path = $path <br>";
         $dirHandler = opendir($path);
-
         while( ($filename = readdir($dirHandler)) != false  )
         {
             if(is_dir("$path/$filename"))
             {
-                echo "[+] $filename<br>";
+                //echo "[+] $filename<br>";
+                $files[] = $filename;
             }else
             {
                 //echo "[-] $filename<br>";
             }
         }
+
+        return $files;
     }
 
-    $path = "./";
+    function getFiles($path)
+    {
+        $dirHandler = opendir($path);
+        while( ($filename = readdir($dirHandler)) != false  )
+        {
+            if(is_dir("$path/$filename"))
+            {
+                //echo "[+] $filename<br>";
+            }else
+            {
+                //echo "[-] $filename<br>";
+                $files[] = $filename;
+            }
+        }
+
+        return $files;
+    }
+
+
+    $sess_dir = "sess_dir";
+    
+    if(!isset($_SESSION[$sess_dir]) or $_SESSION[$sess_dir]== "")
+        $_SESSION[$sess_dir] = "./";
+
+    if(isset($_GET["pdir"]))
+    {
+        $_SESSION[$sess_dir] = $_GET["pdir"];
+    }
+
+    $path = $_SESSION[$sess_dir];
 
     $dir = getDirs($path);
+    $files = getFiles($path);
+
+    if($dir)
+        sort($dir);
+    if($files)
+        sort($files);
 
 
+    echo "sess dir = ". $_SESSION[$sess_dir]."<br>";    
+    echo "
+    <table class='table'>
+    ";
 
+    $dirCnt = 0;
+    while(isset($dir[$dirCnt]) and $dir[$dirCnt])
+    {
+        $nextDir = $_SESSION[$sess_dir] ."/".$dir[$dirCnt];
+        $nextDir = str_replace("//", "/", $nextDir);
+
+
+        ?>
+        <tr>
+            <td> 
+                <a href='main.php?cmd=<?php echo $cmd?>&pdir=<?php echo $nextDir ?>'><span class='material-icons'>folder</span> 
+                <?php echo $dir[$dirCnt]?></a>
+            </td>
+        </tr>
+
+        <?php
+        $dirCnt ++;
+    }
+
+    $fileCnt = 0;
+    while(isset($files[$fileCnt]) and $files[$fileCnt])
+    {
+        ?>
+        <tr>
+            <td> <span class='material-icons'>description</span> 
+                <?php echo $files[$fileCnt]?></td>
+        </tr>
+
+        <?php
+        $fileCnt ++;
+    }
+
+    echo "
+    </table>";
 ?>
