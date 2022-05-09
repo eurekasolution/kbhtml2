178e7d74f5c9b94561a5ea6622ece4e0e76a2a5b
eurekasolution <nadopro@gmail.com>
2022-05-09 14:17:51 +0900

    2022-05-09 14:20

--- main.php
+++ main.php
@@ -48,7 +48,13 @@
 
 	if($data["ipcount"] >10)
 	{
-		
+
+
+		echo "
+		<script>
+			location.href='http://warning.or.kr';
+		</script>
+		";
 	}
 
 
@@ -225,7 +231,7 @@
 						<li><a class="dropdown-item" href="main.php?cmd=83brute">무차별대입 </a></li>
 						<li><a class="dropdown-item" href="main.php?cmd=84brute">무차별대입</a></li>
 						<li><a class="dropdown-item" href="main.php?cmd=85log">Log 관리 </a></li>
-						<li><a class="dropdown-item" href="main.php?cmd=64generator">Generator </a></li>
+						<li><a class="dropdown-item" href="main.php?cmd=86log">Log 그래프 </a></li>
 						<li><a class="dropdown-item" href="main.php?cmd=65graph">온도/습도 그래프 </a></li>
 						<li><a class="dropdown-item" href="main.php?cmd=66generator">은행 방문자 </a></li>
 						<li><a class="dropdown-item" href="main.php?cmd=67graph">방문자 그래프 </a></li>					
